46718bfedbc36614cb874812c23cf6bdcc46c7f4
Rameesalam <128206146+Rameesalam@users.noreply.github.com>
2024-05-22 14:08:39 +0000

    conversion of data

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "ramees"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "ramees"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "ramees" => true
+
+let someNumber = 25
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
