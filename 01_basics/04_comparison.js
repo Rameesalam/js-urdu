b6d70307652052254ef0b552afbde26580acfa65
Rameesalam <128206146+Rameesalam@users.noreply.github.com>
2024-05-22 14:43:33 +0000

    comparison and conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+// need to avoid these type of conversion from line 11 to line 17
+
+// ===
+
+console.log("2" === 2 );
