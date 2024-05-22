75453fb23bad14676aac74bb09f335f762904bc8
Rameesalam <128206146+Rameesalam@users.noreply.github.com>
2024-05-22 13:13:39 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "ramees@google.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "ra@ra.com"
+accountPassword = "123123"
+accountCity = "lahore"
+
+
+console.log(accountId);
+
+/*
+
+not to use var going forward because of functional and block scope
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+ "use strict"; // treat all JS code as newer version
+
+ // alert("hello") // we are using node js, not browser
+
+ console.log(3+3);
+
+ // code readability needs to be high
+
+ console.log("ramees")
+
+ let name = "ramees"
+ let age = 27
+ let isloggedIn = false
+
+  // premitive datatypes   
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+
+ // object 
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); // object
